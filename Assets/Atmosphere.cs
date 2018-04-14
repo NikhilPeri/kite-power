6a737999089eeb3b4f61fc7a9713e673771b6bca
NikhilPeri <nikhil.peri@shopify.com>
2018-04-13 22:15:44 -0400

    initial

---
+++ Assets/Atmosphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atmosphere : MonoBehaviour {
+	public float windStrength;
+	public Vector3 windDirection;
+	public float turbulence;
+
+	public float groundTemprature;
+
+	// meters per second
+	public Vector3 getWindSpeed(Vector3 position) {
+		return windDirection * windStrength * Mathf.Log10(position.y);
+	}
+
+	// degrees C
+	public float getTemprature(Vector3 position){
+		return 15.04f - 0.00649f * position.y;
+	}
+
+	// kPa
+	public float getPressure(Vector3 position){
+		return 101.29f * Mathf.Pow(((getTemprature (position) + 273.1f) / 288.08f), 5.256f);
+	}
+
+	public float getDensity(Vector3 position){
+		return getPressure (position) / (0.2869f * (getTemprature (position) + 273.1f));
+	}
+}
---
+++ Assets/Teather.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teather : MonoBehaviour {
+	public Kite kite;
+	public Generator generator;
+
+	private 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public float getTension(){
+		// ((air_density*(wind_speed**2))/(2))*self.kite_area*cos(self.zenith_ang)*(self.kite_glide_ratio+1)*sqrt((self.kite_glide_ratio**2)+1)
+		return 0.0f;
+	}
+}
